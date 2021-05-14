c0be2909160af2bf6cdccb7dfb0eaef00410d9bd
jacklrs <github@rushsoft.de>
2021-05-14 15:47:33 +0200

    fix: disable "padding" in preview screen

--- app/templates/static/.storybook/preview.js
+++ app/templates/static/.storybook/preview.js
@@ -3,3 +3,5 @@ import { AppDecorator } from "../src/AppDecorator";
 import { useDarkMode } from "storybook-dark-mode";
 
 addDecorator(AppDecorator({ useDarkMode }));
+
+export const parameters = { layout: "fullscreen" };
